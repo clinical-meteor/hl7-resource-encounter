99b08733cb365565bdca19a1a0c81a378b2f40de
Abigail Watson <awatson1978@gmail.com>
2016-09-27 23:17:48 -0500

    bump datatypes version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-encounter',
-  version: '1.1.1',
+  version: '1.1.2',
   summary: 'HL7 FHIR Resource - Encounter',
   git: 'https://github.com/clinical-meteor/hl7-resource-encounter',
   documentation: 'README.md'
@@ -17,7 +17,7 @@ Package.onUse(function (api) {
   api.use('prime8consulting:meteor-oauth2-server@0.0.2');
 
   api.use('clinical:base-model@1.3.5');
-  api.use('clinical:hl7-resource-datatypes@0.4.6');
+  api.use('clinical:hl7-resource-datatypes@0.4.7');
 
   api.addFiles('lib/hl7-resource-encounter.js', ['client', 'server']);
   api.addFiles('server/rest.js', 'server');
