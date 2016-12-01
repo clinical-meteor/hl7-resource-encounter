1496295a6b62a9df46e25258fc8a493fad015bf6
Abigail Watson <awatson1978@gmail.com>
2016-12-01 12:24:29 -0600

    bump deps

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-encounter',
-  version: '1.1.2',
+  version: '1.1.3',
   summary: 'HL7 FHIR Resource - Encounter',
   git: 'https://github.com/clinical-meteor/hl7-resource-encounter',
   documentation: 'README.md'
@@ -12,17 +12,18 @@ Package.onUse(function (api) {
   api.use('meteor-platform');
   api.use('mongo');
   api.use('aldeed:simple-schema@1.3.3');
-  api.use('aldeed:collection2@2.3.3');
+  api.use('aldeed:collection2@2.5.0');
   api.use('simple:json-routes@2.1.0');
   api.use('prime8consulting:meteor-oauth2-server@0.0.2');
 
   api.use('clinical:base-model@1.3.5');
-  api.use('clinical:hl7-resource-datatypes@0.4.7');
+  api.use('clinical:hl7-resource-datatypes@0.6.0');
 
   api.addFiles('lib/hl7-resource-encounter.js', ['client', 'server']);
   api.addFiles('server/rest.js', 'server');
   api.addFiles('server/initialize.js', 'server');
 
+  api.export('Encounter');
   api.export('Encounters');
   api.export('EncounterSchema');
 });
