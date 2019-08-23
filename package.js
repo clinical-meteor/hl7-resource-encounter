8e37a763262bb4e2a3bd37e62efa557650fb0bfd
Abigail Watson <awatson1978@gmail.com>
2019-08-23 17:09:35 -0500

    simpl-schema upgrade

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-encounter',
-  version: '1.2.1',
+  version: '1.2.2',
   summary: 'HL7 FHIR Resource - Encounter',
   git: 'https://github.com/clinical-meteor/hl7-resource-encounter',
   documentation: 'README.md'
@@ -11,14 +11,31 @@ Package.onUse(function (api) {
 
   api.use('meteor-platform');
   api.use('mongo');
-  api.use('aldeed:simple-schema@1.3.3');
-  api.use('aldeed:collection2@2.5.0');
+
+  api.use('aldeed:collection2@3.0.0');
   api.use('simple:json-routes@2.1.0');
-  api.use('clinical:fhir-vault-server@0.0.3');
 
-  api.use('clinical:base-model@1.3.5');
-  api.use('clinical:hl7-resource-datatypes@3.0.0');
+  api.use('simple:json-routes@2.1.0');
+  api.use('momentjs:moment@2.17.1');
+  api.use('ecmascript@0.9.0');
+  api.use('session');
+  api.use('http');
+  api.use('react-meteor-data@0.2.15');
+  
+  api.use('clinical:hl7-resource-datatypes@4.0.0');
+  api.use('clinical:hl7-resource-bundle@1.5.5');
 
+  if(Package['clinical:fhir-vault-server']){
+    api.use('clinical:fhir-vault-server@0.0.3', ['client', 'server'], {weak: true});
+  }
+
+  api.use('clinical:glass-ui@2.4.7');
+  api.use('clinical:extended-api@2.4.0');
+  api.use('clinical:base-model@1.4.0');
+  api.use('clinical:user-model@1.6.2');
+  api.use('matb33:collection-hooks@0.7.15');
+  api.use('clinical:alert@2.1.3');
+  
   api.addFiles('lib/hl7-resource-encounter.js', ['client', 'server']);
   api.addFiles('server/rest.js', 'server');
   api.addFiles('server/initialize.js', 'server');
@@ -26,4 +43,11 @@ Package.onUse(function (api) {
   api.export('Encounter');
   api.export('Encounters');
   api.export('EncounterSchema');
+});
+
+Npm.depends({
+  "simpl-schema": "1.5.3",
+  "moment": "2.22.2",
+  "validator": "10.9.0",
+  "lodash": "4.17.4"
 });
